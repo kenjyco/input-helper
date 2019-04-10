6b2721550c75535977b947b6f127cea73a6b8d70
Ken <kenjyco@gmail.com>
2019-04-10 00:39:15 -0500

    Bump to v0.1.32

--- setup.py
+++ setup.py
@@ -6,14 +6,14 @@ with open('README.rst', 'r') as fp:
 
 setup(
     name='input-helper',
-    version='0.1.31',
+    version='0.1.32',
     description='Common CLI input helper functions and string/arg conversions',
     long_description=long_description,
     author='Ken',
     author_email='kenjyco@gmail.com',
     license='MIT',
     url='https://github.com/kenjyco/input-helper',
-    download_url='https://github.com/kenjyco/input-helper/tarball/v0.1.31',
+    download_url='https://github.com/kenjyco/input-helper/tarball/v0.1.32',
     packages=find_packages(),
     setup_requires=['pytest-runner'],
     tests_require=['pytest'],
