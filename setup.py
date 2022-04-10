a57caae9c4f9356e9e43e0da49c2563b6b4d5dc4
Ken <kenjyco@gmail.com>
2022-04-10 16:57:11 -0500

    Add extras_require section to setup for ipython/xmljson/full

--- setup.py
+++ setup.py
@@ -4,6 +4,12 @@ from setuptools import setup, find_packages
 with open('README.rst', 'r') as fp:
     long_description = fp.read()
 
+with open('requirements-ipython.txt', 'r') as fp:
+    requirements_ipython = fp.read().splitlines()
+
+with open('requirements-xmljson.txt', 'r') as fp:
+    requirements_xmljson = fp.read().splitlines()
+
 setup(
     name='input-helper',
     version='0.1.41',
@@ -15,6 +21,11 @@ setup(
     url='https://github.com/kenjyco/input-helper',
     download_url='https://github.com/kenjyco/input-helper/tarball/v0.1.41',
     packages=find_packages(),
+    extras_require={
+        'ipython': requirements_ipython,
+        'xmljson': requirements_xmljson,
+        'full': requirements_ipython + requirements_xmljson,
+    },
     setup_requires=['pytest-runner'],
     tests_require=['pytest'],
     classifiers=[
